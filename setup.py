d7e2c37bc813120ebd1cf8f3c80471570dcdc602
Atharva Hatekar <atharva.hatekar@metso.com>
2025-07-19 13:03:42 +0200

    package created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='document_portal',
+    version='0.1',
+    author='Atharva Hatekar',
+    author_email='atharva_hatekar@yahoo.in',
+    packages=find_packages(),
+)
